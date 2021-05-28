cb58a50ce1cb3c52b706c22b084fa234890b5d3d
Emerson Gomes Rebolsas <emerson.cursos.ti@gmail.com>
2021-05-28 20:38:19 -0300

    Typando o  Componente Main.

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,6 +4,7 @@ import * as S from './styles'
 interface Props {
   children: React.ReactNode;
 }
+
 const Main: React.FC<Props> = ({ children  }) => <S.Main>{children}</S.Main>
 
 export default Main

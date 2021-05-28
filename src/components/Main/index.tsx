d82fc23313a987504e6f41135df25edfe607c0a7
Emerson Gomes Rebolsas <emerson.cursos.ti@gmail.com>
2021-05-28 20:01:36 -0300

    Resolvendo erros no Componente Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,22 +2,6 @@ import React from 'react'
 
 import * as S from './styles'
 
-const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, React, NextJS e Styled Components'
-}) => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela de código. "
-    />
-  </S.Wrapper>
-)
+const Main = ({ children }) => <S.Main>{children}</S.Main>
 
 export default Main
